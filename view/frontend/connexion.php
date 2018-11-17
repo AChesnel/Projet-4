83c8ca29357b7ff57ae527fb1cc1a0bb39d3ec71
AChesnel <39329134+AChesnel@users.noreply.github.com>
2018-11-17 16:43:57 +0100

    Refonte graphique page login + Nouvelle font

--- view/frontend/connexion.php
+++ view/frontend/connexion.php
@@ -1,13 +1,17 @@
 <?php include('header.php');?>
-<div id="blocConnexion">
-	<form action="index.php?action=login" method="post">
+<div id="blocSite">
+	<h1>Connexion</h1>
+	<div id="backgroundBlocConnexion">
+		<div id="blocConnexion">
+			<form action="index.php?action=login" method="post">
 
-		<input type="text" name="identifiant" id="identifiant" placeholder="identifiant" required="required"> <br />
-		<input type="password" name="password" id="password" placeholder="Mot de passe" required="required">
-
-		<input type="submit">
-	</form>
-
-	<a href="http://localhost/projet4/index.php?action=nouveauCompte">Créer un compte</a>
+				<input type="text" name="identifiant" id="identifiant" placeholder="Identifiant" required="required" class="elementForm">
+				<input type="password" name="password" id="password" placeholder="Mot de passe" required="required" class="elementForm">
+				<input type="submit" class="elementForm boutonEnvoyer">
+			</form>
+			
+			<a href="http://localhost/projet4/index.php?action=nouveauCompte">Créer un compte</a>
+		</div>
+	</div>
 </div>
 <?php include('footer.php');?>
