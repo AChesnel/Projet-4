8dfb8ff5476db385ba057f8008bd8016b912be09
AChesnel <39329134+AChesnel@users.noreply.github.com>
2019-01-03 16:48:26 +0100

    Correction pagination

--- view/frontend/pageArticles.php
+++ view/frontend/pageArticles.php
@@ -7,7 +7,7 @@
 	<h1>Articles</h1>
 		<div class="articles">
 			<?php  foreach ($articles as $article) :?>
-				<div id="bulleArticle">
+				<div class="bulleArticle">
 				    	<a href="http://localhost/projet4/index.php?action=article&amp;id=<?= $article['id'] ?>"><h2 id="titrePageArticle"><?= htmlspecialchars($article['title']) ?></h2></a>
 
 				    	<?= htmlspecialchars(substr($article['content'], 493,350)) ?> <a href="http://localhost/projet4/index.php?action=article&amp;id=<?= $article['id'] ?>">Lire la suite...</a>
